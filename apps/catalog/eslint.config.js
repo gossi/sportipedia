6e54866082a32ac932576156a66fcc9bcc6d1c7c
gossi <gossi@users.noreply.github.com>
2026-06-29 14:52:03 +0200

    Revert "update infra deps"
    This reverts commit 07d5b253f042d32fc4fa58f88267d178568934be.

--- apps/catalog/eslint.config.js
+++ apps/catalog/eslint.config.js
@@ -1,11 +1,3 @@
 import ember from '@gossi/config-eslint/ember';
 
-export default [
-  ...ember(import.meta.dirname),
-  {
-    files: ['src/**/*.ts'],
-    rules: {
-      'unicorn/consistent-class-member-order': 'off',
-    }
-  }
-];
+export default ember(import.meta.dirname);
